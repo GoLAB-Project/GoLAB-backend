2ada04547dea714b73344bfaf60a511bfac6172a
Mukho <mukho@kakao.com>
2023-07-19 16:57:08 +0900

    Disable spring security's login page

--- src/main/java/com/golab/talk/TalkApplication.java
+++ src/main/java/com/golab/talk/TalkApplication.java
@@ -2,8 +2,9 @@ package com.golab.talk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TalkApplication {
 
 	public static void main(String[] args) {
