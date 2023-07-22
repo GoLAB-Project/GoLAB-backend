97521bdfb76f5d5e68d9bbb6c62317c77526e071
Mukho <mukho@kakao.com>
2023-07-22 22:28:05 +0900

    Feat: 차단 목록 기본 기능 구현
    - DTO, Repository, Service, Controller 작성

---
+++ src/main/java/com/golab/talk/dto/BanListDto.java
@@ -0,0 +1,17 @@
+package com.golab.talk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BanListDto {
+
+	private String userId;
+	private String banId;
+
+}
