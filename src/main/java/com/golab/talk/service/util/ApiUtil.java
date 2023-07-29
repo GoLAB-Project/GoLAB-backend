c9d71b493cb202743f567a086b8ba9f05a14c8c5
Subny <richsubin411@gmail.com>
2023-07-30 04:43:11 +0900

    [FEAT] API관련 utill 추가

---
+++ src/main/java/com/golab/talk/service/util/ApiUtil.java
@@ -0,0 +1,98 @@
+package com.golab.talk.service.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Random;
+
+public class ApiUtil {
+
+	public Gson gson = new Gson();
+
+	public String getAccessToken(String accessTokenUrl) {
+		// Access token by post
+		HttpEntity<String> entity = new HttpEntity<>("", new HttpHeaders());
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.postForEntity(accessTokenUrl, entity,
+			String.class);
+
+		// Parsing response.body from string to json object
+		JsonObject tokenObject = gson.fromJson(response.getBody(), JsonObject.class);
+		String accessToken = tokenObject.get("access_token").getAsString();
+		return accessToken;
+	}
+
+	// 네이버 회원 프로필 조회 API 명세: https://developers.naver.com/docs/login/profile/profile.md
+	public String get(String apiUrl, Map<String, String> requestHeaders) {
+		HttpURLConnection con = connect(apiUrl);
+		try {
+			con.setRequestMethod("GET");
+			for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
+				con.setRequestProperty(header.getKey(), header.getValue());
+			}
+
+			int responseCode = con.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+				return readBody(con.getInputStream());
+			} else { // 에러 발생
+				return readBody(con.getErrorStream());
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("API 요청과 응답 실패", e);
+		} finally {
+			con.disconnect();
+		}
+	}
+
+	public HttpURLConnection connect(String apiUrl) {
+		try {
+			URL url = new URL(apiUrl);
+			return (HttpURLConnection)url.openConnection();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+		} catch (IOException e) {
+			throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+		}
+	}
+
+	public String readBody(InputStream body) {
+		InputStreamReader streamReader = new InputStreamReader(body);
+
+		try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+			StringBuilder responseBody = new StringBuilder();
+
+			String line;
+			while ((line = lineReader.readLine()) != null) {
+				responseBody.append(line);
+			}
+
+			return responseBody.toString();
+		} catch (IOException e) {
+			throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+		}
+	}
+
+	public String generateRandomUserStr(int length) {//랜덤 아이디, state 만들어줌
+		Random random = new Random();
+		String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < length; ++i) {
+			sb.append(characters.charAt(random.nextInt(characters.length())));
+		}
+		return sb.toString();
+	}
+
+
+}
