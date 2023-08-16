ade096b9cf943ace4801d894a8ee522850aca09a
Mukho <mukho@kakao.com>
2023-08-17 03:56:20 +0900

    Feat: SocketHandler 수정 - 기존 SocketHandle는 본인의 채팅을 다시 돌려받는 echo 방식이었음. - 현재 닉네임 없는 익명 채팅처럼, 본인을 제외한 모든 세션에게 텍스트 데이터를 전송함

--- src/main/java/com/golab/talk/websocket/SocketHandler.java
+++ src/main/java/com/golab/talk/websocket/SocketHandler.java
@@ -1,17 +1,44 @@
 package com.golab.talk.websocket;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Component
 public class SocketHandler extends TextWebSocketHandler {
+
+	private Set<WebSocketSession> sessions = new HashSet<>();
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		super.afterConnectionEstablished(session);
+		sessions.add(session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		super.afterConnectionClosed(session, status);
+		sessions.remove(session);
+	}
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		String receivedMessage = message.getPayload();
-		
+
 		// 원하는 처리 로직을 구현
-		session.sendMessage(new TextMessage(receivedMessage));
+
+		// 본인에게 다시 보내기
+		//session.sendMessage(new TextMessage(receivedMessage));
+
+		// 현재 연결된 모든 클라이언트에 메시지 전송
+		for (WebSocketSession currentSession : sessions) {
+			currentSession.sendMessage(new TextMessage(receivedMessage));
+		}
 	}
 
 }
