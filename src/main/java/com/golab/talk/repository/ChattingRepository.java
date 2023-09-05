0959aaab1a757f609e1c4d1c21d33808b24c0699
Mukho <mukho@kakao.com>
2023-09-05 23:13:35 +0900

    Chore: 수행할 작업에 대한 주석 추가 - From desktop to macbook 백업용입니다. - 구현 중 구조에 대한 변경점이 생길 예정입니다.

--- src/main/java/com/golab/talk/repository/ChattingRepository.java
+++ src/main/java/com/golab/talk/repository/ChattingRepository.java
@@ -13,4 +13,18 @@ public interface ChattingRepository extends JpaRepository<Chatting, Integer> {
 
 	List<Chatting> findByIdLessThanAndRoomIdOrderByIdDesc(int cursor, int roomId, Pageable pageable);
 
+	// Repository + Service + Controller 로직 정비
+
+	// 채팅 생성 -> {roomId, sendUserId, message, 1, datetime, datetime}
+	// 채팅 정보 업데이트 -> (updateTime? 관련 로직 + sendUserId가 내 id와 다를 때) notRead가 1 이상이라면 notRead -= 1 {notRead : 0, updatedAt : datetime}
+	// 채팅방 정보 업데이트 -> identifier("1-2")를 통해 id를 추출하고, 두 아이디의 채팅방에 대해 {lastChat : message, updatedAt : datetime}
+
+	// (발신)채팅을 보냈을 때
+	// 채팅 생성
+	// 채팅방 정보 업데이트
+
+	// (수신)채팅을 읽었을 때
+	// 채팅 정보 업데이트
+	// 채팅방 정보 업데이트
+
 }
