d9ee52e8c6989fda1ea33ff686ca96458eb8d6a6
skybluelion <brotherjun96@gmail.com>
2023-09-28 12:48:15 +0900

    [Feat] Topic 관련 파일 수정 - TopicController, Scheduler, TopicRepository, TopicService, TopicServiceImpl 수정 / 주제 초기화 기능 구현

--- src/main/java/com/golab/talk/repository/TopicRepository.java
+++ src/main/java/com/golab/talk/repository/TopicRepository.java
@@ -1,6 +1,11 @@
 package com.golab.talk.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.golab.talk.domain.DiscussionTopic;
@@ -8,4 +13,17 @@ import com.golab.talk.domain.DiscussionTopic;
 @Repository
 public interface TopicRepository extends JpaRepository<DiscussionTopic, Long> {
 
+	// 게임방 리스트
+	@Query(value = "SELECT * FROM discussion_topic", nativeQuery = true)
+	List<DiscussionTopic> showTopicList();
+
+	//토픽 초기화
+	@Modifying
+	@Query(value = "DELETE FROM discussion_topic A WHERE A.topic_id in :topicId", nativeQuery = true)
+	void deleteTopic(@Param("topicId") List<Integer> topicId);
+
+	//토픽 번호 초기화
+	@Modifying
+	@Query(value = "ALTER TABLE discussion_topic AUTO_INCREMENT = 1", nativeQuery = true)
+	void resetTopicId();
 }
